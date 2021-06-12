3c6d08ea901e1b784bbf283100620575199bf7ef
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-06-12 23:43:14 +0530

    added: created linked list and added elements

--- datastructures/LinkedList.java
+++ datastructures/LinkedList.java
@@ -1,8 +1,31 @@
+/************************************************************
+ * Purpose Implementation Of Linked List
+ * @author Rosy Rupali
+ * @Version 1.0
+ * @since 12-06-2021
+ *
+ ************************************************************/
 package com.bridgelabz.datastructures;
 
 public class LinkedList<T> {
 
-	public static void main(String[] args) {
-		System.out.println("Linked list using Java Generics");
+	Node<T> head;
+
+	/**
+	 * add elements to the linked list
+	 * @param data
+	 */
+	public void add(T data) {
+		Node<T> newNode = new Node<>();
+		newNode.data = data;
+		if (head == null) {
+			head = newNode;
+		} else {
+			Node<T> node = head;
+			while (node.next != null) {
+				node = node.next;
+			}
+			node.next = newNode;
+		}
 	}
 }
---
+++ datastructures/Node.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.datastructures;
+
+public class Node<T> {
+	T data;
+	Node<T> next;
+
+}
