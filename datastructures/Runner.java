9e256fa6846e5ff98eafb802494e572b58c81f0d
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-06-13 07:38:46 +0530

    added: different sequence for adding elements

--- datastructures/Runner.java
+++ datastructures/Runner.java
@@ -7,9 +7,9 @@ public class Runner {
 	public static void main(String[] args) {
 		
 		LinkedList<Integer> numberList = new LinkedList<Integer>();
-		numberList.addAtStart(70);
-		numberList.addAtStart(30);
-		numberList.addAtStart(56);
+		numberList.add(56);
+		numberList.add(30);
+		numberList.add(70);
 		numberList.show();
 
 
